a37a28536b0976cd714c05c1c868bafd7313aa74
EdenBrodbeck <gabrielagimenez13@hotmail.com>
2014-10-17 00:49:21 -0430

    Update testEstacionamiento.py

--- testEstacionamiento.py
+++ testEstacionamiento.py
@@ -1,13 +1,52 @@
-
 import unittest
-import estacionamiento as es
+from estacionamiento import intentarEstacionar
 
 class Test(unittest.TestCase):
+
+##############################################################################
+#        PRUEBA FUNCION 1 intentarEstacionar
+############################################################################
+
+    #Se omite el primer TDD, funcion vacia
     
-    def testName(self):
-        pass
+    #Estacionamiento: 1 puesto, Estado: Libre, Hora: 0 == "6:00"
+    def testreservaPuesto_Cero(self):
+        estadoEstacionamiento = [[0]]
+        horaLlegada = 0  
+        placa = 13 
+        placaPuesto={}
+        resul = {'estadoEstacionamiento': [[1]], 'hayPuesto': True, 'placaPuesto': {13: 0}}
+        self.assertEqual(resul,intentarEstacionar(estadoEstacionamiento, placa, horaLlegada, placaPuesto))
 
-
+    #Estacionamiento: 1 puesto, Estado: Ocupado, Hora: 0 == "6:00"
+    def testreservaPuesto_Ocu1(self):
+        estadoEstacionamiento = [[1]]
+        horaLlegada = 0
+        placa = 13
+        placaPuesto={}
+        resul = {'estadoEstacionamiento': [[1]], 'hayPuesto': False, 'placaPuesto': {}}
+        self.assertEqual(resul,intentarEstacionar(estadoEstacionamiento, placa, horaLlegada, placaPuesto))
+    
+    #Estacionamiento: 1 puesto, Estado: Reservado Desocupado, Hora: 0 == "6:00"
+    def testreservaPuesto_Ocu2(self):
+        estadoEstacionamiento = [[2]]
+        horaLlegada = 0 
+        placa = 13
+        placaPuesto={}
+        resul = {'estadoEstacionamiento': [[2]], 'hayPuesto': False, 'placaPuesto': {}}
+        self.assertEqual(resul,intentarEstacionar(estadoEstacionamiento, placa, horaLlegada, placaPuesto))
+        
+    #Estacionamiento: 1 puesto, Estado: Reservado desocupado, Hora: 0 == "6:00"
+    def testreservaPuesto_Ocu3(self):
+        estadoEstacionamiento = [[3]]
+        horaLlegada = 0 
+        placa = 13
+        placaPuesto={}
+        resul = {'estadoEstacionamiento': [[3]], 'hayPuesto': False, 'placaPuesto': {}}
+        self.assertEqual(resul,intentarEstacionar(estadoEstacionamiento, placa, horaLlegada, placaPuesto))   
+ 
+        
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
